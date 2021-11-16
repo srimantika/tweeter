12cecfb9f3b32e14fac3618347caac418338311a
srimantika <srimantika@gmail.com>
2021-11-16 18:46:33 +0000

    Added javascript to handle tweet text counter if it exceeds the character limit of 140

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+
+// Check character count of tweet text and becomes negative red counter once it exceeds 140 length
+$(document).ready(function() {
+  const $input = $(".textArea");
+  $input.on("keyup", (event) => {
+    let tweetlength = event.target.value.length;
+    //console.log(tweetlength);
+    $(".counter").text (140 - tweetlength);
+    if (tweetlength > 140 ) {
+      $(".counter").css("color", "red");
+    } else {
+      $(".counter").css("color", "black");
+    }
+  });
+});
