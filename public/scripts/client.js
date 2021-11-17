473b014d823801370e6aae9f0edc3653eee85f0a
srimantika <srimantika@gmail.com>
2021-11-17 19:09:22 +0000

    Changed functionality and templates to add dynamic tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,6 +7,37 @@
  */
 
 $(document).ready(function() {
+  const tweetObject = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1636997380182
+  }
+
+ function createTweetElement(tweetObject) {
+  const $tweet = $(`<h3><img src=${tweetObject.user.avatars}> ${tweetObject.user.name} <span>${tweetObject.user.handle}</span></h3><p>${tweetObject.content.text}</p> <div>10 days ago  <i id = "retweet" class="fa-solid fa-retweet"></i> <i id = "heart" class="fa-solid fa-heart"></i> <i id = "flag" class="fa-solid fa-flag"></i></div>`);
+  return $tweet;    
+  }
+
+
+    $('#tweetdisplay').html(createTweetElement(tweetObject));
+
+
+
+
+
+
+
+
+
+
+  //--------------------------------------------------------------------------------------
+// Code for box shadow and color change on mouse hover  
   const $tweets = $("#tweets")
   $tweets.on('mouseover', (event) => {
   document.getElementById("tweets").style.boxShadow = "10px 20px 30px"
