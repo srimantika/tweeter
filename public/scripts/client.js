0a6077d99c82c9f7a1cd6b8e36af0f4826e454f9
srimantika <srimantika@gmail.com>
2021-11-18 20:01:09 +0000

    Look and feel

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -40,7 +40,7 @@ $(document).ready(function() {
     return $tweet;    
   }
 
-//Escape function to avoid cross scripting
+//Escape function to avoid cross scripting issues
   const escape = function (str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
