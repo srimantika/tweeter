2c15a8d87989bbcf7a65cd44508fb887e4638ede
srimantika <srimantika@gmail.com>
2021-11-18 01:53:20 +0000

    added validation and error message

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,31 +8,9 @@
 
 $(document).ready(function() {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-  
+  const data = [];
+
+   
   function createTweetElement(tweetObject) {
     const $tweet = $(`
     <article class="tweet">
@@ -63,6 +41,24 @@ $(document).ready(function() {
 
 
 
+  $( ".submit-tweet" ).submit(function( event ) {
+    event.preventDefault();
+
+    let inputLength = $("textArea").val().length;
+    console.log(inputLength);
+    if(inputLength === 0 || inputLength === null || inputLength > 140) {
+      $(".error-container").slideDown();
+      $(".error-message").html("<h5>Error: Please enter a valid tweet!</h5>");
+    } else {
+      $(".error-container").slideUp();  
+      const tweetContent = $(".submit-tweet").serialize();
+      $.post("/tweets", tweetContent, function () {
+      $("textArea").val("");
+      loadTweets();
+    })
+  }
+  }); 
+
   function loadTweets() {
     $.ajax({
       url: "/tweets",
@@ -74,13 +70,6 @@ $(document).ready(function() {
       }
     });
   }
-  loadTweets();
-
-  $( ".submit-tweet" ).submit(function( event ) {
-    alert( "Handler for .submit() called." );
-    event.preventDefault();
-    loadTweets();
-  });
 
   function renderTweets(tweetsArray) {
     for (let value of tweetsArray) {
@@ -88,7 +77,5 @@ $(document).ready(function() {
       $('#tweets-container').prepend($tweet);
     }
   }
- 
-
 
 });
